f78113e12df4894f59eacf7b38794a1341676906
Aymeric PINEAU <ay.pineau@gmail.com>
2026-03-18 02:03:38 -0600

    chore: update dependencies and improve configuration formatting

--- intlayer.config.ts
+++ intlayer.config.ts
@@ -1,10 +1,14 @@
 import { type IntlayerConfig, Locales } from "intlayer";
 
 const config: IntlayerConfig = {
-	internationalization: {
-		locales: [Locales.ENGLISH, Locales.FRENCH, Locales.SPANISH],
-		defaultLocale: Locales.ENGLISH,
-	},
+  internationalization: {
+    locales: [Locales.ENGLISH, Locales.FRENCH, Locales.SPANISH],
+    defaultLocale: Locales.ENGLISH,
+  },
+  editor: {
+    enabled: true,
+    applicationURL: "http://localhost:3000",
+  },
 };
 
 export default config;
