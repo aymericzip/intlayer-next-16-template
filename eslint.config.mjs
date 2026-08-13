a34a351c3d313d8f02daa3fdbb07e65ef649634d
aymericzip <ay.pineau@gmail.com>
2026-08-12 18:18:15 -0600

    chore: update eslint configuration and devDependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import intlayer from "eslint-plugin-intlayer";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  ...intlayer,
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
